1141ab93bf1aae24c01ad0fdf0cfd9d57d640342
akerele abraham <abrahamakerele38@gmail.com>
2019-10-17 05:50:25 +0100

    WIP: fixing stuff

--- TestMath.py
+++ TestMath.py
@@ -1,25 +1,31 @@
+#!/bin/bash
+
 import unittest
-from math import tokenize,processString
+from math import result,globMultipleOperation
 
 tokensandresult={
-            "1+22+33":["1","+","22","+","33"],
-            "1+2+(4+4)":["1","+","2","+","(4+4)"],
-            "(1+2+33)":["(1+2+33)"],
-            "13*(2+5*(2-3))":["13","*","(2+5*(2-3))"]
+            "1+22+33":"56",
+            "1+2+(4+4)":"11",
+            "(1+2+33)":"36",
+            "13*(2+5*(2-3))":"-39",
+            "1*(5^6)":"15625"
 }
 
 class TestMath(unittest.TestCase):
-    def test_tokenize(self):
-        
-        for k in tokensandresult:
-            token=tokenize(k)
-            print(token,tokensandresult[k])
-            self.assertEqual(token, tokensandresult[k]) 
+    
+    def testGlobMultiple(self):
+        testCases=[
+                [['3', '*', '2', '+', '5', '*', '-', '1.0'],7],
+                [['2', '-', '3'],3],
+                [['1','+', '22', '+', '3'],5]
+            ]
+        for case in testCases:
+            self.assertEquals(len(globMultipleOperation(case[0])),case[1])
+
     def test_result(self):
-        
         for k in tokensandresult:
-            token=processString(k)
-            print(token)
+            token=result(k)
+            self.assertEqual(token,tokensandresult[k])
 
 if __name__ == '__main__': 
     unittest.main()
--- math.py
+++ math.py
@@ -9,10 +9,37 @@ operations={
     "*":lambda x,y:x*y,
     "^":lambda x,y:x**y
 }
+pattern = re.compile(r'\((\d+\.?\d?[*/^+-]?)*\)') #match the smallest open and closing brace
+subBracketPattern = r'(\d+(?=[*/^+-]|$)|\d+\.\d+|[*/^+-])'
+
+
 def removeWhiteSpace(command):
     return re.sub("\s","",command)
     
+isFloat = lambda x: x.isdigit() and float(x)-int(x)>0
+
+def globMultipleOperation(tokens):
+    returnTokes=[]
+    addMe=""
+
+    for t in range(len(tokens)-1):
+        # :(
+        if tokens[t]=="-":
+            if not (tokens[t+1] in operations) and  tokens[t-1] in operations:
+                addMe="-"
+            else:
+                returnTokes.append(str(tokens[t]))
+        else:
+            returnTokes.append(addMe+str(tokens[t]))
+            addMe=""
+    returnTokes.append(addMe+str(tokens[-1]))
+    return returnTokes
+
+
+
 def performOperation(tokens,operate):
+    tokens=globMultipleOperation(tokens)
+
     stack=[]
     lentokens=len(tokens)
     index=0
@@ -37,33 +64,49 @@ def processString(command):
         if command[-1]!=")":
             raise  ValueError("invalid command")
         command=command[1:lenghts-1] # remove the braces
-    tokens = re.findall(r'(\d+(?=[*/^+-]|$)|\d+\.\d+|[*/^+-])', command)    
-    #tokens = re.findall(r'(\d+|[*/^+-])', command)     #using bodmas rule solve division first then multiplication 
-    # then addition then subtraction
-
+    tokens = re.findall(subBracketPattern, command)
+    print(tokens,"tokw")
+   
+    
     for op in ["^","/","*","+","-"]:
-        tokens=performOperation(tokens,op) 
+        tokens=performOperation(tokens,op)
+    
     return  tokens[0]
+
 def processBraces(string): 
+    #base case
     try:
         float(string)
-        print(string)
-        return
+        return string
     except:
         pass
-    pattern = re.compile(r'\((\d+\.?\d?[*/^+-]?)*\)') #match the smallest open and closing brace
+    
     match = re.search(pattern, string) 
-    if match: 
-        ans = processString(match.group()) 
+    print(string)
+    if match:
+        print(match)
+        ans = processString(match.group())
+        
         newstring = re.sub(pattern,str(ans),string,1)
+     
+        
     else: 
         #no brace found, so process string like that
         newstring = str(processString(string)) 
-    processBraces(newstring) 
+    return processBraces(newstring) 
  
+def result(string):
+    res=processBraces(string)
+    if not isFloat(res):
+        return res.split(".")[0]
+    else:
+        return res
+
 args=sys.argv
 string=""
 if __name__=="__main__":
     for i in args[1:]:
         string+=i
-    processBraces(string) 
+    print(result(string))
+    
+       
